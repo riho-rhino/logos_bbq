e02026b7b805fe59d6826507ef28fea0ad82d170
Riho yamamoto <r.yamamoto@rhino-inc.jp>
2025-05-20 17:34:33 +0900

    testup

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,3 +2,109 @@ import $ from "jquery"
 import modal from "./modal"
 modal()
 
+opning();
+function tick(time) {
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            resolve()
+        }, time)
+    })
+}
+async function opning() {
+$('body').addClass('noscroll')
+  await tick(100);
+  $('.bbq-op').addClass('show');
+  await tick(4000);
+//   await tick(10);
+  $('.bbq-op').fadeOut(1000);
+  $('body').removeClass('noscroll');
+  await tick(900);
+  $('.bbq_mv').addClass('start');
+  $('.bbq_left').addClass('start');
+  $('.bbq_right').addClass('start');
+  $('.bbd_lead').addClass('start');
+}
+
+// アンカーリンク
+$(window).on('load', function () {
+  $('.pc-anchor-inner a[href*="#"]').on('click', function () {
+    var elmHash = $(this).attr('href');
+    var pos = $(elmHash).offset().top - 100;
+    $('html, body').animate({ scrollTop: pos }, 600);
+    return false;
+  });
+});
+// アンカーリンク
+$(window).on('load', function () {
+  $('.sp-anchor a[href*="#"]').on('click', function () {
+    var elmHash = $(this).attr('href');
+    var pos = $(elmHash).offset().top - 100;
+    $('html, body').animate({ scrollTop: pos }, 600);
+    return false;
+  });
+});
+
+
+$(function () {
+    $(window).on({
+      'scroll': function () {
+        scrollFadeIn();
+        scrollActive();
+        movieIn();
+        scrollActivedelay();
+        scrollFadeIndelay();
+      },
+    });
+  })
+  
+  function scrollActive() {
+    $('.move').each(function () {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height() * 0.7;
+  
+      if (elementTop < viewportBottom) {
+        $(this).addClass('active');
+      }
+    });
+  }
+  function scrollActivedelay() {
+    $('.move-delay').each(function () {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height() * 0.6;
+  
+      if (elementTop < viewportBottom) {
+        $(this).addClass('active');
+      }
+    });
+  }
+  
+  function scrollFadeIn() {
+    $('.bbq-fadein').each(function () {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height() * 0.7;
+  
+      if (elementTop < viewportBottom) {
+        $(this).addClass('in');
+      }
+    });
+  }
+  function scrollFadeIndelay() {
+    $('.bbq-fadein-delay').each(function () {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height() * 0.6;
+  
+      if (elementTop < viewportBottom) {
+        $(this).addClass('in');
+      }
+    });
+  }
+  function movieIn() {
+    $('.lookmovie-wrap').each(function () {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height() * 0.75;
+  
+      if (elementTop < viewportBottom) {
+        $(this).addClass('in');
+      }
+    });
+  }
--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,37 +1,51 @@
-export default()=>{
-    
-    $('.lookmovie-wrap').click(function() {
+export default () => {
+
+    // ボタンがクリックされたとき
+    $('.lookmovie-wrap').on('click', function () {
+        $('body').addClass('noscroll');
         var videoID = $(this).data('video-id'); // data-video-idからYouTubeのIDを取得
         var videoURL = "https://www.youtube.com/embed/" + videoID + "?autoplay=1"; // 自動再生を追加
         var productLink = $(this).data('product-link'); // 商品詳細リンクを取得
-    
-        // モーダルを表示
-        $('.bbq_modal-wrap').show();
+        // モーダルラップをフェードインで表示
+        $('.bbq_modal-wrap').fadeIn(300, function() {
+            // .bbq-youtubeにfadeInアニメーションを適用
+            $('.bbq_modal-inner').css('animation', 'fadeIn 0.7s forwards');
+        });
+        var iframe = $('<iframe>', {
+            src: videoURL,
+            frameborder: 0,
+            allowfullscreen: true,
+            allow: 'autoplay',
+            class: 'youtube-iframe'
+        });
+
+        $('.bbq-modal .bbq-youtube').html(iframe);  
         
-        // モーダル内のYouTube iframeに動画URLを設定
-        $('.bbq-modal .youtube').html('<iframe src="' + videoURL + '" frameborder="0" allowfullscreen></iframe>');
-        
-        // 商品詳細リンクを設定
         $('.bbq-modal .product-link').attr('href', productLink);
         
-        // 0.5秒後に動画を自動再生させる
         setTimeout(function() {
-          // モーダルが表示された後に動画の自動再生を開始
-          var iframe = $('.bbq-modal .youtube iframe')[0];
-          iframe.src = iframe.src; // 再読み込みで再生を開始
-        }, 500); // 500ミリ秒後に再生開始
-      });
-    
-      // モーダルを閉じる
-      $('.bbq_modal-close').click(function() {
-        $('.bbq_modal-wrap').hide(); // モーダルを非表示にする
-        $('.bbq-modal .youtube').html(''); // YouTubeのiframeをリセット
-      });
-    // モーダルを閉じる
-    $('.bbq_modal-bg').click(function() {
-        $('.bbq_modal-wrap').hide(); // モーダルを非表示にする
-        $('.bbq-modal .youtube').html(''); // YouTubeのiframeをリセット
+            var iframeElement = $('.bbq-modal .bbq-youtube iframe')[0];
+            iframeElement.src = iframeElement.src; 
+        }, 300); 
     });
-      
-      
+
+    $('.bbq_modal-bg').on('click', function () {
+        $('body').removeClass('noscroll');
+        $('.bbq_modal-inner').css('animation', 'fadeOut 0.5s forwards'); 
+        setTimeout(function() {
+            $('.bbq_modal-wrap').fadeOut(300); 
+        }, 500); 
+        $('.bbq-modal .bbq-youtube').html(''); 
+    });
+
+    // モーダルを閉じる（フェードアウト）
+    $('.bbq_modal-close').on('click', function () {
+        $('body').removeClass('noscroll');
+        $('.bbq_modal-inner').css('animation', 'fadeOut 0.5s forwards');  // fadeOutアニメーションを適用
+        setTimeout(function() {
+            $('.bbq_modal-wrap').fadeOut(300); // モーダルをラップを非表示にする
+        }, 500); // fadeOutが終了するまで待機
+        $('.bbq-modal .bbq-youtube').html(''); 
+    });
+
 }
